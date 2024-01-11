603d1fe919ce73c28be9268197c6b87b5542f336
evgtz <e.t@cek.ru>
2024-01-11 16:03:05 +0300

    initWifi increase init time

--- lib/wifi/cekwifi.cpp
+++ lib/wifi/cekwifi.cpp
@@ -7,7 +7,7 @@ bool initWifi(String ssid, String password){
   uint trys = 5;
   WiFi.begin(ssid, password);             // Connect to the network
   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(500);
+    delay(1000);
     Serial.print('.');
     if(!trys--){
       Serial.println("Connection failed");
