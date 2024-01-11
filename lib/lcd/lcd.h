6f72eca4e56d0dd41bdb7b9efd0df47a52cf828e
evgtz <e.t@cek.ru>
2024-01-11 16:02:29 +0300

    temp close lcd

--- lib/lcd/lcd.h
+++ lib/lcd/lcd.h
@@ -3,18 +3,18 @@
 #define _CEK_ESP_LCD_H_
 
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+// #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4); 
+// LiquidCrystal_I2C lcd(0x27,20,4); 
 
-void initLCD(){
-  #define I2C_SDA 21
-  #define I2C_SCL 22
+// void initLCD(){
+//   #define I2C_SDA 21
+//   #define I2C_SCL 22
 
-  Wire.begin(I2C_SDA, I2C_SCL);
+//   Wire.begin(I2C_SDA, I2C_SCL);
 
-  lcd.init();
-  lcd.clear();
-  lcd.backlight();
-}
+//   lcd.init();
+//   lcd.clear();
+//   lcd.backlight();
+// }
 #endif
